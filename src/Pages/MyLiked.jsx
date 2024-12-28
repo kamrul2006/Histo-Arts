9fa29d8e10aa9c9dd154e7c983b5636fb5290206
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2024-12-28 22:40:45 +0600

    My liked page with Token verf added

--- src/Pages/MyLiked.jsx
+++ src/Pages/MyLiked.jsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 import useAxiosSecure from "../hooks/useAxiocSec";
+import nodata from "../assets/nodata.jpg"
+
+
 
 const MyLiked = () => {
     const axiosSec = useAxiosSecure()
@@ -10,69 +13,75 @@ const MyLiked = () => {
     useEffect(() => {
         axiosSec.get(`/liked?email=${user.email}`)
             .then(res => setData(res.data))
-
     }, [])
-
 
 
     return (
         <div>
 
-            <div className="overflow-x-auto w-full">
-                <table className="table w-full">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Artifact Name</th>
-                            <th>Details</th>
-                            <th>Added By</th>
-                            <th>Unlike</th>
-                        </tr>
-                    </thead>
 
-
-                    <tbody>
-
-                        {data.map(D =>
-                            <tr key={D._id}>
-                                <td>
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle h-12 w-12">
-                                            <img
-                                                src={D.data.artifactImage}
-                                                alt="Artifact" />
-                                        </div>
-                                    </div>
-                                </td>
-
-                                <td>
-                                    <p className="text-lg font-semibold">{D.data.artifactName}</p>
-                                </td>
-
-                                <td className="text-xs text-gray-500">
-                                    <p>Discoverd By: {D.data.discoveredBy}</p><hr />
-                                    <p>Discoverd At: {D.data.discoveredAt}</p><hr />
-                                    <p>Location: {D.data.presentLocation}</p>
-                                </td>
-
-                                <td className="text-xs italic text-gray-300">
-                                    Added By: <br />
-                                    {D.data.adderInfo.name}
-                                </td>
-
-                                <th>
-                                    <button className="btn btn-info btn-outline btn-xs">UnLike</button>
-                                </th>
+            {data.length == 0 ?
+                <div>
+                    <h1 className='md:text-4xl text-2xl font-mono font-black text-center mt-10'>No Liked Artifacts.</h1>
+                    <img src={nodata} className='mx-auto w-1/2' />
+                </div> :
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Artifact Name</th>
+                                <th>Details</th>
+                                <th>Added By</th>
+                                <th>Unlike</th>
                             </tr>
-                        )}
+                        </thead>
 
 
-                    </tbody>
-                </table>
-            </div>
+                        <tbody>
 
-        </div>
+                            {data.map(D =>
+                                <tr key={D._id}>
+                                    <td>
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={D.data.artifactImage}
+                                                    alt="Artifact" />
+                                            </div>
+                                        </div>
+                                    </td>
+
+                                    <td>
+                                        <p className="text-lg font-semibold">{D.data.artifactName}</p>
+                                    </td>
+
+                                    <td className="text-xs text-gray-500">
+                                        <p>Discoverd By: {D.data.discoveredBy}</p><hr />
+                                        <p>Discoverd At: {D.data.discoveredAt}</p><hr />
+                                        <p>Location: {D.data.presentLocation}</p>
+                                    </td>
+
+                                    <td className="text-xs italic text-gray-300">
+                                        Added By: <br />
+                                        {D.data.adderInfo.name}
+                                    </td>
+
+                                    <th>
+                                        <button className="btn btn-info btn-outline btn-xs">UnLike</button>
+                                    </th>
+                                </tr>
+                            )}
+
+
+                        </tbody>
+                    </table>
+                </div>
+            }
+
+
+        </div >
     );
 };
 
