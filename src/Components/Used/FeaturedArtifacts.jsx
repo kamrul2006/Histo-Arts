71955d870a570b5f92d3f694c94ab4eaf7018533
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2025-02-12 00:03:19 +0600

    Most liked page update (u-1)

--- src/Components/Used/FeaturedArtifacts.jsx
+++ src/Components/Used/FeaturedArtifacts.jsx
@@ -23,11 +23,11 @@ const FeaturedArtifacts = () => {
                 Here are some of our most liked arts. to see all arts click on the view all Art button aor for details click on View Details .
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-7 mx-5 md:mx-10">
-                {crafts.slice(0,6).map(Craft =>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 mx-5 md:mx-10">
+                {crafts.slice(0,8).map(Craft =>
                     <div key={Craft._id}>
                         <Slide>
-                            <div className="card card-compact bg-base-100 w-96 shadow-xl">
+                            <div className="card card-compact bg-base-100  shadow-xl">
                                 <figure>
                                     <img
                                         src={Craft.artifactImage}
